21bc8e1fdd71618b79552c86e7738c8c48f99eca
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2021-03-04 13:04:27 +0100

    Ugrade

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+var items = ["Training", "Drink Water", "Do Yoga"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	var today = new Date();
@@ -13,7 +17,14 @@ app.get("/", (req, res) => {
 	};
 	var day = today.toLocaleDateString("en-US", options);
 
-	res.render("list", { kindOfDay: day });
+	res.render("list", { kindOfDay: day, newListItems: items });
+});
+app.post("/", (req, res) => {
+	var item = req.body.newItem;
+
+	items.push(item);
+
+	res.redirect("/");
 });
 
 app.listen("3000", (req, res) => {
