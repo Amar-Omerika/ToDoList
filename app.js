2d752db1f6ec0aca9da00d91f21a7b3f0ac19075
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2021-03-06 22:19:51 +0100

    ToDoList

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 const path = require("path");
 const app = express();
 
@@ -14,14 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	let today = new Date();
-	let options = {
-		weekday: "long",
-		day: "numeric",
-		month: "long",
-	};
-	let day = today.toLocaleDateString("en-US", options);
-
+	let day = date();
 	res.render("list", { listTitle: day, newListItems: items });
 });
 app.post("/", (req, res) => {
