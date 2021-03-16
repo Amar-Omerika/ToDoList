3632b25e2c83f4292a133bfa0c3185f3eda357b8
Amar Omerika <amar.omerika@edu.fit.ba>
2021-03-16 17:33:30 +0100

    ToDoList

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("moongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
 	name: String,
