b24515d18d07782d27be04e7421beb4c48f3c1c0
Amar Omerika <amar.omerika@edu.fit.ba>
2021-03-16 17:11:11 +0100

    ToDoList

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 const mongoose=require("mongoose");
 
 
@@ -16,8 +15,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect("moongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
+const itemsSchema = {
+	name: String,
+}
+const Item = mongoose.model("Item", itemsSchema);
+
 app.get("/", (req, res) => {
-	const day = date.getDate();
+
 	res.render("list", { listTitle: day, newListItems: items });
 });
 app.post("/", (req, res) => {
