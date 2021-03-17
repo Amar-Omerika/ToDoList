075f1dbd600d6cac4a0237a3ec515e71dc58916e
Amar Omerika <amar.omerika@edu.fit.ba>
2021-03-17 17:22:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose=require("mongoose");
-
+const mongoose = require("mongoose");
 
 const path = require("path");
 const app = express();
-
-
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 const itemsSchema = {
 	name: String,
-}
+};
 const Item = mongoose.model("Item", itemsSchema);
 
-const item1=new Item({
-	name:"Welcome to your todolist."
+const item1 = new Item({
+	name: "Welcome to your todolist.",
 });
-const item2=new Item({
-	name:"Hit the + button to aff a new item."
+const item2 = new Item({
+	name: "Hit the + button to aff a new item.",
 });
-const item3=new Item({
-	name:"<-- Hit this to delete an item."
+const item3 = new Item({
+	name: "<-- Hit this to delete an item.",
 });
 
-const defaultItems = [item1,item2,item3];
+const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems,function(err){
-	if(err){
+Item.insertMany(defaultItems, function (err) {
+	if (err) {
 		console.log(err);
-
-	}else{
-		console.log("Success");
+	} else {
+		console.log("Successfully connected to database");
 	}
-})
+});
 
 app.get("/", (req, res) => {
-
-	res.render("list", { listTitle: "Today", newListItems: items });
+	Item.find({}, function (err, foundItems) {
+		res.render("list", { listTitle: "Today", newListItems: foundItems });
+	});
 });
 app.post("/", (req, res) => {
 	const item = req.body.newItem;
