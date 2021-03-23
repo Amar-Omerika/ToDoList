30d85b3d1d462cc207c85e2ecb32a92057dee653
Amar Omerika <amar.omerika@edu.fit.ba>
2021-03-23 12:46:05 +0100

    ToDoList

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+mongoose.connect(
+	"mongodb+srv://admin-amar:<password>@cluster0.upceh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
 
 const itemsSchema = {
 	name: String,
