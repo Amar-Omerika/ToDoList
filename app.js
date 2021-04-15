89d4fd11279e46f7984a104a6ea137274ec84450
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2021-04-15 11:18:15 +0200

    ToDoList

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
-
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/secrets"
 mongoose.connect(
-	"mongodb+srv://admin-amar:password@cluster0.upceh.mongodb.net/todolistDB",
+	dbUrl,
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
